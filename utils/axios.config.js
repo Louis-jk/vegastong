3a47f4c0a6460a0af3a1880b4b082ccdb035e4e2
Juno Kim <lippoint.surf0622@gmail.com>
2022-06-20 15:18:49 +0900

    log 주석처리

--- utils/axios.config.js
+++ utils/axios.config.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
-import Config from 'react-native-config';
+import axios from 'axios'
+import Config from 'react-native-config'
 
-axios.defaults.baseURL = Config.BASE_URL;
+axios.defaults.baseURL = Config.BASE_URL
 
 axios.defaults.headers.post['Content-Type'] =
-  'application/x-www-form-urlencoded';
-axios.defaults.headers.post['api-secret'] = Config.API_SECRET;
+  'application/x-www-form-urlencoded'
+axios.defaults.headers.post['api-secret'] = Config.API_SECRET
 
 export const VegasPost = (url, data, header) => {
-  console.log('VegasPost header ??', header);
+  // console.log('VegasPost header ??', header);
   return new Promise((resolve, reject) => {
     axios
       .post(url, data, header)
       .then((res) => {
-        resolve(res.data);
+        resolve(res.data)
       })
       .catch((err) => {
-        reject(err);
-        console.log(err);
-      });
-  });
+        reject(err)
+        console.log(err)
+      })
+  })
 };
 
 export const VegasGet = (url, header) => {
@@ -27,11 +27,11 @@ export const VegasGet = (url, header) => {
     axios
       .get(url, header || null)
       .then((res) => {
-        resolve(res.data);
+        resolve(res.data)
       })
       .catch((err) => {
-        reject(err);
-        console.log(err);
-      });
-  });
+        reject(err)
+        console.log(err)
+      })
+  })
 };
